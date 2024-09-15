61164ed3ef62a0f17e9391e7c0460408a1c1639a
Deivid21 <david.parra.ignacio@gmail.com>
2024-09-15 04:08:13 -0300

    kiev: Build MotCamera3 from prebuilt source
    Change-Id: Ib9ed2ab3e3a101026577a02786890871d80b1a2e
    Signed-off-by: Deivid21 <david.parra.ignacio@gmail.com>

--- device.mk
+++ device.mk
@@ -97,6 +97,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
     $(LOCAL_PATH)/configs/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
 
+# Moto Camera 3
+TARGET_MOTCAMERA3 := kiev
+TARGET_USES_MOTCAMERA3 := true
+
+$(call inherit-product, vendor/motorola/MotCamera3/motcamera3.mk)
+
 # NFC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
